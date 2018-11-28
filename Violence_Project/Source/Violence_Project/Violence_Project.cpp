35721a1df706a583f617677092ba349b81db14e8
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2018-11-28 10:18:26 +0000

    Health regen
    Some health regen functionality in blueprints needs some work!

---
+++ Violence_Project/Source/Violence_Project/Violence_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Violence_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Violence_Project, "Violence_Project" );
